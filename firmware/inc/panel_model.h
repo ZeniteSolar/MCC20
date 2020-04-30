74f7dff46e81a9eb726d2c7b9305806a9de72b6b
João Antônio Cardoso <joao.maker@gmail.com>
2020-04-29 22:13:16 -0300

    panel_model moved to panel_model.[hc]

---
+++ firmware/inc/panel_model.h
@@ -0,0 +1,6 @@
+#ifndef _PANEL_MODEL_H
+#define _PANEL_MODEL_H 
+
+double panel_model(double Vpv);
+
+#endif /* ifndef _PANEL_MODEL_H */
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,26 +5,8 @@
 #include "main.h"
 #include "control.h"
 #include "solvers.h"
+#include "panel_model.h"
 
-
-static inline double panel_model(double Vpv)
-{
-  const double Voc = 40;
-  const double Isc = 8;
-  const double G = 1000;
-  double Ipv;
-
-  if (Vpv >= Voc)
-    Ipv = 0;
-  else {
-    Ipv = 0.5 * Isc * (G / 1000.0) *
-          (1.0 + ((Vpv - Voc + 1.0) / (Vpv - Voc - 1.0)));
-    if (Ipv < 0)
-      Ipv = 0;
-  }
-
-  return Ipv;
-}
 
 static inline double fun_circuit(double x, const double *params)
 {
