7183be18c82f9fc6cf4bf8200ed7e43050280a2a
João Antônio Cardoso <joao.maker@gmail.com>
2020-05-29 12:04:56 -0300

    Makefile added

--- firmware/run.sh
+++ firmware/run.sh
@@ -1,38 +1,9 @@
 #!/bin/sh
 
 # Clean
-\rm -rf bin *_socket
+\rm -rf *_socket
 
-# Prepare
-\mkdir -p bin
-
-# Build
-FLAGS="
-    -std=gnu99 -O2 \
-    -pedantic -Wall -Wextra \
-    -Wconversion \
-    -Wshadow \
-    -Wunsafe-loop-optimizations \
-    -Waddress \
-    -Wlogical-op \
-    -Wstrict-prototypes \
-    -Wold-style-definition \
-    -Wmissing-prototypes \
-    -Wmissing-declarations \
-    -Wpacked \
-    -Wundef \
-    -Wredundant-decls \
-    -Wnested-externs \
-    -Wunreachable-code \
-    -Winline \
-    -Woverlength-strings \
-    -Wpointer-sign \
-    -Wdisabled-optimization \
-    -Wvariadic-macros \
-    -fstrict-aliasing \
-    -Wstrict-overflow=5 -fstrict-overflow
-"
-gcc ${FLAGS} -Iinc/ src/*.c -obin/main &&
+make
 
 # Run    
 ./bin/main
